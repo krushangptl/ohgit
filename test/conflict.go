7693f98b1b4883ce796fae19eba9215447ac75dc
Krushang D Patel <krushang4201@gmail.com>
2024-10-11 14:29:21 +0530

    Resolve Problem that happen because of Mistake

--- test/conflict.go
+++ test/conflict.go
@@ -5,29 +5,16 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
 	fmt.Println("Merge Conflict!")
-=======
 	fmt.Println("Conflict!")
->>>>>>> conflicts
-
-	// root function call
-	root()
-}
-
-func root() {
-<<<<<<< HEAD
-	// rooted to main
 	fmt.Println("This is IMPORTANT CODE!")
-=======
-	fmt.Println("This is IMPORTANT CODE!")
-	fmt.Println("Ignore!")
 	fmt.Println("Don't Ignore!")
     fmt.Println(repo(45))
+	// root function call
+	root()
 }
 
 // exp code 
 func repo(v int) int{
     return v+=2;
->>>>>>> conflicts
 }
